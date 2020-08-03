90dbceb52056ea89c80afd283c74cb1ed4985c88
Kevin messmer <kevin.messmer@yahoo.com>
2020-08-04 01:30:19 +0200

    graph for diff between original and interpolate

--- src/co2_consumption/views.py
+++ src/co2_consumption/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 from src.co2_consumption.models import ConsumptionInterpolate, Consumption
 import pandas as pd
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
 
 
 class IndexView(TemplateView):
@@ -19,5 +21,23 @@ class IndexView(TemplateView):
         df_interpolated = pd.DataFrame(
             ConsumptionInterpolate.objects.all().order_by('datetime').values()
         ).set_index('datetime').resample('30T').asfreq().interpolate()
+
+        # Using ffill() to fill the last data of the dataframe.
+        df_diff = df_original['co2_rate'].sub(
+            df_interpolated['co2_rate']
+        ).ffill()
+
+        context['graph_diff'] = plot([Scatter(
+                x=df_original.groupby(
+                    pd.Grouper(freq='15D')
+                ).mean().index.tolist(),
+                y=df_diff,
+                mode='lines',
+                name='graph difference between original and interpolated',
+                opacity=0.8,
+                marker_color='green'
+            )],
+            output_type='div'
+        )
 
         return context
